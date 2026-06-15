30a8c737d8a3695ee1f601027ffc8b357fadc798
RafaVetto <vettori_rafa@yahoo.com.br>
2026-06-15 17:01:23 -0300

    niver

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date ("Dec 6, 2026 19:00:00");
+const dataDoEvento = new Date ("Nov 18, 2026 18:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
